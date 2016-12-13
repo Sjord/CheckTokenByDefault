a7d4985f73354428c4672837f190f3acf10e65c9
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2016-12-13 13:34:12 +0100

    Add project files.

---
+++ CheckTokenByDefault/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CheckTokenByDefault.Startup))]
+namespace CheckTokenByDefault
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
